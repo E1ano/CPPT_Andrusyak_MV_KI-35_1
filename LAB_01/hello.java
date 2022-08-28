37ab0ecb150d05d5da1ec60a94c035a7d117196e
Marko <andrusyakmarko28@gmail.com>
2022-08-28 21:56:19 +0300

    LAB_01 first commit.

---
+++ LAB_01/hello.java
@@ -0,0 +1,9 @@
+public class Hello
+{
+    public static void main(String[] args)
+    {
+        String message = new String();
+        message = "Hello, Marko Andrusiak, KI-35";
+        System.out.println(message);
+    }
+}
